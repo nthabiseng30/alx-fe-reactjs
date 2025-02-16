53e32c72a536f9cc62e552f3fd2ea37b2714f13d
Abdul-Rahman <bodysttar@gmail.com>
2025-02-16 15:13:54 +0200

    Third Commit.

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -13,8 +13,8 @@ function App() {
   return (
     <>
       <div>
-        <WelcomeMessage/>
-        <Header/>
+        <WelcomeMessage />
+        <Header />
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -24,14 +24,14 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <MainContent/>
+        <MainContent />
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
-        <Footer/>
+        <Footer />
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more

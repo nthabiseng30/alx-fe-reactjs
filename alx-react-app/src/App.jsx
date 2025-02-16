a083655ca41bdc5e31f3fff2906760434f300904
Abdul-Rahman <bodysttar@gmail.com>
2025-02-16 14:57:00 +0200

    Third Commit.

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -3,13 +3,18 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import WelcomeMessage from './components/WelcomeMessage';
+import Header from './Header';
+import MainContent from './MainContent';
+import Footer from './Footer';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <WelcomeMessage />
+      <Header/>
+      <WelcomeMessage/>
+      <MainContent/>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -30,6 +35,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <Footer/>
     </>
   )
 }

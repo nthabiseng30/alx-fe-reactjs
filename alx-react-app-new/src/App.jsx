13994bce670f98afc49a50e7a328f547d15b81a0
Abdul-Rahman <bodysttar@gmail.com>
2025-02-23 10:26:06 +0200

    Sixth Commit.

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -4,6 +4,7 @@ import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
 import UserProfile from './components/UserProfile';
+import Counter from './components/Counter';
 function App() {
   return (
     <>
@@ -12,6 +13,7 @@ function App() {
         <Header />
         <MainContent />
         <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+        <Counter/>
         <Footer />
       </div>
     </>

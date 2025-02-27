c35fdbc703e600a62622787e81617df34c0be403
nthabiseng mahlangu <nthabisengmahlangu139@gmail.com>
2025-02-27 22:40:26 +0200

    Update UserDetails.jsx

--- alx-react-app-prop/src/components/UserDetails.jsx
+++ alx-react-app-prop/src/components/UserDetails.jsx
@@ -1,10 +1,13 @@
-/* eslint-disable */
-function UserDetails({ userData }) {
-    return (
-      <div>
-        <p>Name: {userData.name}</p>
-        <p>Email: {userData.email}</p>
-      </div>
-    );
+import React, { useContext } from 'react';
+import UserContext from './UserContext';
+
+function UserDetails() {
+  const userData = useContext(UserContext);
+
+  return (
+    <div>
+      <p>Name: {userData.name}</p>
+      <p>Email: {userData.email}</p>
+    </div>
+  );
 }
-export default UserDetails;

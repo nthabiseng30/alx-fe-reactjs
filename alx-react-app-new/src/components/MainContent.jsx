d8467c8c36c42b80db3586ddeb637e597e32edc9
nthabiseng mahlangu <nthabisengmahlangu139@gmail.com>
2025-02-27 14:03:10 +0200

    Update MainContent.jsx

--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -1,7 +1,13 @@
 export default function MainContent(){
-    return (
-    <main style={{ backgroundColor: 'green', color: 'black', textAlign: 'center' }}>
-        <p>I love to visit New York, Paris, and Tokyo.</p>
+  return (
+    <main style={{
+      backgroundColor: 'green',
+      color: 'black',
+      textAlign: 'center',
+      padding: '20px',
+      fontSize: '18px'
+    }}>
+      <p>I love to visit New YorK, Paris, and San Francisco.</p>
     </main>
   );
 };

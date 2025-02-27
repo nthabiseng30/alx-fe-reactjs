2180c57d551473e9022504c5d1eb3774df34edf0
nthabiseng mahlangu <nthabisengmahlangu139@gmail.com>
2025-02-27 22:31:42 +0200

    Update App.jsx

--- alx-react-app-prop/src/App.jsx
+++ alx-react-app-prop/src/App.jsx
@@ -1,13 +1,17 @@
-import { UserContext } from './components/UserContext';
 import ProfilePage from './ProfilePage';
+import UserContext from './UserContext';
+
 function App() {
-  const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
+  const userData = {
+    name: 'Jane Doe',
+    email: 'jane.doe@example.com',
+  };
+
   return (
-    <><UserContext.Provider value={userData}>
-      <ProfilePage userData={userData} />
-      </UserContext.Provider>
-    </>
-    
+    <UserContext.Provider value={userData}>
+      <ProfilePage />
+    </UserContext.Provider>
   );
 }
+
 export default App;

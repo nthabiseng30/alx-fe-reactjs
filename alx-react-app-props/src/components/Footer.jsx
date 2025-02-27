19d7021845e44d82e452508a19d022a3ea097d5f
nthabiseng mahlangu <nthabisengmahlangu139@gmail.com>
2025-02-28 01:30:46 +0200

    Update Footer.jsx

--- alx-react-app-props/src/components/Footer.jsx
+++ alx-react-app-props/src/components/Footer.jsx
@@ -1,1 +1,12 @@
-
+export default function Footer(){
+  return (
+    <footer style={{
+      backgroundColor: 'yellow', // changed to yellow
+      color: 'brown', // changed to brown
+      textAlign: 'center',
+      padding: '10px' // added padding
+    }}>
+      <p>&copy; 2023 City Lovers</p> // updated copyright text
+    </footer>
+  );
+};

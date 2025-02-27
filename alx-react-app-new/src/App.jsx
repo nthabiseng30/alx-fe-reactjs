0d0c752a2250dd2e6d5faf50d5e33c1541bd8890
nthabiseng mahlangu <nthabisengmahlangu139@gmail.com>
2025-02-27 17:05:38 +0200

    Update App.jsx

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -5,26 +5,18 @@ import MainContent from './components/MainContent';
 import Footer from './components/Footer';
 import UserProfile from './components/UserProfile';
 import Counter from './components/Counter';
-import UserContext from './UserContext';
-
 function App() {
-  const userData = {
-    name: 'Nthabiseng Mahlangu',
-    email: 'nthabisengmahlangu493@gmail.com',
-  };
-
   return (
-    <UserContext.Provider value={userData}>
+    <>
       <div>
         <WelcomeMessage />
         <Header />
         <MainContent />
-        <UserProfile />
+        <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
         <Counter/>
         <Footer />
       </div>
-    </UserContext.Provider>
-  );
+    </>
+  )
 }
-
-export default App;
+export default App

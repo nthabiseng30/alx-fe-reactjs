1b15a33a8479ea865755731e72930fc5db115e67
nthabiseng mahlangu <nthabisengmahlangu139@gmail.com>
2025-02-28 01:59:49 +0200

    Update App.jsx

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,22 +1,18 @@
-import './App.css'
-import WelcomeMessage from './components/WelcomeMessage';
-import Header from './components/Header';
-import MainContent from './components/MainContent';
-import Footer from './components/Footer';
-import UserProfile from './components/UserProfile';
-import Counter from './components/Counter';
+import ProfilePage from './ProfilePage';
+import UserContext from './UserContext';
+
 function App() {
+  const userData = {
+    name: 'Jane Doe',
+    email: 'jane.doe@example.com'
+  };
+
   return (
-    <>
-      <div>
-        <WelcomeMessage />
-        <Header />
-        <MainContent />
-        <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
-        <Counter/>
-        <Footer />
-      </div>
-    </>
-  )
+    <UserContext.Provider value={userData}>
+      <ProfilePage />
+    </UserContext.Provider>
+  );
 }
-export default App
+
+export default App;
+

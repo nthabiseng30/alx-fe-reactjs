e5e40c7cd82f4e288a921bda94c7ac4d61aebf0f
nthabiseng mahlangu <nthabisengmahlangu139@gmail.com>
2025-02-27 23:15:01 +0200

    Delete alx-react-app-prop directory

--- alx-react-app-prop/src/components/WelcomeMessage.jsx
+++
@@ -1,9 +0,0 @@
-export default function WelcomeMessage() {
-    return (
-        <div>
-            <h1>Hello everyone, I am learning React at ALX!</h1>
-            <p>This is a simple JSX component.</p>
-            <p>I am learning about JSX!</p>
-        </div>
-    );
-};

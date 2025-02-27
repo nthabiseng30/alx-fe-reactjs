be633fe6a9dc7d529331d5da78c5510fa5f9f1b4
nthabiseng mahlangu <nthabisengmahlangu139@gmail.com>
2025-02-27 22:56:14 +0200

    Update UserDetails.jsx

--- alx-react-app-prop/src/components/UserDetails.jsx
+++ alx-react-app-prop/src/components/UserDetails.jsx
@@ -1,15 +1,7 @@
-import React, { useContext } from 'react';
-import UserContext from './UserContext';
+import UserProfile from './UserProfile';
 
 function UserDetails() {
-  const userData = useContext(UserContext);
-
-  return (
-    <div>
-      <p>Name: {userData.name}</p>
-      <p>Email: {userData.email}</p>
-    </div>
-  );
+  return <UserProfile />;
 }
 
 export default UserDetails;

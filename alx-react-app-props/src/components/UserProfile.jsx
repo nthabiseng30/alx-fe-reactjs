e9c1111e6ae82ecd7500b7a360c7bcf4a08a6b94
nthabiseng mahlangu <nthabisengmahlangu139@gmail.com>
2025-02-28 02:08:38 +0200

    Update UserProfile.jsx

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,15 +1,7 @@
-/* eslint-disable */
-function UserProfile(props){
-  return (
-    <div style={{
-      border: '1px solid gray',
-      padding: '10px',
-      margin: '10px'
-    }}>
-      <h2 style={{ color: 'blue' }}>{props.name}</h2>
-      <p>Age: <span style={{ fontWeight: 'bold' }}>{props.age}</span></p>
-      <p>Bio: {props.bio}</p>
-    </div>
-  );
+import UserDetails from './UserDetails';
+
+function UserProfile() {
+  return <UserDetails />;
 }
+
 export default UserProfile;

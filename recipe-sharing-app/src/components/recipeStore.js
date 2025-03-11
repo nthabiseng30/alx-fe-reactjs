464ecba2c24435b44eda43c210eec458ecce5ecf
nthabiseng mahlangu <nthabisengmahlangu139@gmail.com>
2025-03-12 01:53:09 +0200

    Update recipeStore.js

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -1,7 +1,47 @@
-const useRecipeStore = create(set => ({
-  recipes: [],
-  addRecipe: (newRecipe) => set(state => ({ recipes: [...state.recipes, newRecipe] })),
-  setRecipes: (recipes) => set({ recipes })
-})));
+import { createContext, useReducer } from 'react';
 
-export default useRecipeStore;
+// Define the initial state
+const initialState = {
+  recipes: [],
+  selectedRecipe: null,
+};
+
+// Define the reducer function
+const recipeReducer = (state, action) => {
+  switch (action.type) {
+    case 'ADD_RECIPE':
+      return { ...state, recipes: [...state.recipes, action.recipe] };
+    case 'DELETE_RECIPE':
+      return {
+        ...state,
+        recipes: state.recipes.filter(recipe => recipe.id !== action.id),
+      };
+    case 'EDIT_RECIPE':
+      return {
+        ...state,
+        recipes: state.recipes.map(recipe =>
+          recipe.id === action.id ? { ...recipe, ...action.changes } : recipe
+        ),
+      };
+    case 'SELECT_RECIPE':
+      return { ...state, selectedRecipe: action.recipe };
+    default:
+      return state;
+  }
+};
+
+// Create the context
+const RecipeContext = createContext();
+
+// Create the provider component
+const RecipeProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(recipeReducer, initialState);
+
+  return (
+    <RecipeContext.Provider value={{ state, dispatch }}>
+      {children}
+    </RecipeContext.Provider>
+  );
+};
+
+export { RecipeProvider, RecipeContext };

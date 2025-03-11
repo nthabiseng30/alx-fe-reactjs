fbdae3a757a269a854a19d1e0ea782bc92081c5e
nthabiseng mahlangu <nthabisengmahlangu139@gmail.com>
2025-03-11 13:37:24 +0200

    Create recipeList.js

---
+++ recipe-sharing-app/src/components/recipeList.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import useRecipeStore from '../recipeStore';
+
+const RecipeList = () => {
+  const recipes = useRecipeStore(state => state.recipes);
+
+  return (
+    <div>
+      <h2>Recipe List</h2>
+      <ul>
+        {recipes.map(recipe => (
+          <li key={recipe.id}>
+            <h3>{recipe.title}</h3>
+            <p>{recipe.description}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default RecipeList;

105b3c94276bddca8d28fdf03fdc1fd8c9617905
nthabiseng mahlangu <nthabisengmahlangu139@gmail.com>
2025-03-15 01:27:16 +0200

    Create Home.jsx

---
+++ my-company/src/Home.jsx
@@ -0,0 +1,14 @@
+// src/Home.jsx
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to My Company</h1>
+      <p>This is the homepage.</p>
+    </div>
+  );
+};
+
+export default Home;
+

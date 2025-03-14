d4ed14b3bc729f8c69de795b32f123af4f30c22f
nthabiseng mahlangu <nthabisengmahlangu139@gmail.com>
2025-03-15 01:29:27 +0200

    Create  Contact.jsx

---
+++ my-company/src/Contact.jsx
@@ -0,0 +1,13 @@
+// src/Contact.jsx
+import React from 'react';
+
+const Contact = () => {
+  return (
+    <div>
+      <h1>Get in Touch</h1>
+      <p>This is the contact page.</p>
+    </div>
+  );
+};
+
+export default Contact;

61476d4c91704f97abf1f592ed2b1e489db75009
nthabiseng mahlangu <nthabisengmahlangu139@gmail.com>
2025-03-15 01:56:42 +0200

    Update App.jsx

--- form-handling-react/src/App.jsx
+++ form-handling-react/src/App.jsx
@@ -1,8 +1,10 @@
+// App.jsx
 import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
-import RegistrationForm from './components/RegistrationForm'; // Import the RegistrationForm component
+import RegistrationForm from './components/RegistrationForm';
+import FormikForm from './formikForm'; // Import the FormikForm component
 
 function App() {
   const [count, setCount] = useState(0);
@@ -23,7 +25,8 @@ function App() {
         </button>
         <p>Edit <code>src/App.jsx</code> and save to test HMR</p>
       </div>
-      <RegistrationForm /> {/* Add the RegistrationForm component */}
+      <RegistrationForm />
+      <FormikForm /> {/* Add the FormikForm component */}
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>

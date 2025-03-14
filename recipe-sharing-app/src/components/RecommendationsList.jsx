89dc7efc3f4072d5d1e489540a3f74cf02adffc0
nthabiseng mahlangu <nthabisengmahlangu139@gmail.com>
2025-03-15 00:39:01 +0200

    Create RecommendationsList.jsx

---
+++ recipe-sharing-app/src/components/RecommendationsList.jsx
@@ -0,0 +1,25 @@
+// src/components/RecommendationsList.jsx
+import React, { useEffect } from 'react';
+import { useRecipeStore } from '../stores/recipeStore';
+
+const RecommendationsList = () => {
+  const { recommendations, generateRecommendations } = useRecipeStore();
+
+  useEffect(() => {
+    generateRecommendations();
+  }, [generateRecommendations]);
+
+  return (
+    <div>
+      <h2>Recommended Recipes</h2>
+      {recommendations.map(recipe => (
+        <div key={recipe.id}>
+          <h3>{recipe.title}</h3>
+          <p>{recipe.description}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default RecommendationsList;

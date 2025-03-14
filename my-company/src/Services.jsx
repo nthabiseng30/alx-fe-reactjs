d9a9bc8926785fc475bd2746f5dfacd125adf0a0
nthabiseng mahlangu <nthabisengmahlangu139@gmail.com>
2025-03-15 01:28:49 +0200

    Create Services.jsx

---
+++ my-company/src/Services.jsx
@@ -0,0 +1,14 @@
+// src/Services.jsx
+import React from 'react';
+
+const Services = () => {
+  return (
+    <div>
+      <h1>Our Services</h1>
+      <p>This is the services page.</p>
+    </div>
+  );
+};
+
+export default Services;
+

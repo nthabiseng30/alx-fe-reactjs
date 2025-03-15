3687809829356b6d23c10bd996954e11ad7476c5
nthabiseng mahlangu <nthabisengmahlangu139@gmail.com>
2025-03-15 17:29:14 +0200

    Update App.jsx

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -1,24 +1,15 @@
-// src/App.jsx
+// App.jsx
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './Home';
-import About from './About';
-import Services from './Services';
-import Contact from './Contact';
-import Navbar from './Navbar';
+import FormikForm from './formikForm'; // Import the FormikForm component
 
-const App = () => {
+function App() {
   return (
-    <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/services" element={<Services />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-    </BrowserRouter>
+    <div>
+      {/* Your existing code here */}
+      <FormikForm /> // Add the FormikForm component here
+    </div>
   );
-};
+}
 
 export default App;
+

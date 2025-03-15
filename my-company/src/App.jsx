f05edb22f6d84c2c1059ca39fa659e16103b9a27
nthabiseng mahlangu <nthabisengmahlangu139@gmail.com>
2025-03-15 17:30:21 +0200

    Update App.jsx

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -1,15 +1,1 @@
-// App.jsx
-import React from 'react';
-import FormikForm from './formikForm'; // Import the FormikForm component
-
-function App() {
-  return (
-    <div>
-      {/* Your existing code here */}
-      <FormikForm /> // Add the FormikForm component here
-    </div>
-  );
-}
-
-export default App;
 

afdc7cee8c80b8cde5f82c94922b1c9d35a4b68f
nthabiseng mahlangu <nthabisengmahlangu139@gmail.com>
2025-03-16 22:35:21 +0200

    Update App.jsx

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,27 +1,18 @@
 // App.jsx
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import ProtectedRoute from './components/ProtectedRoute';
-import Profile from './components/Profile';
-import Login from './components/Login';
+import Profile from './Profile';
+import BlogPost from './BlogPost';
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route
-          path="/profile"
-          element={
-            <ProtectedRoute>
-              <Profile />
-            </ProtectedRoute>
-          }
-        />
+        <Route path="/profile/*" element={<Profile />} />
+        <Route path="/blog/:id" element={<BlogPost />} />
       </Routes>
     </BrowserRouter>
   );
 };
 
 export default App;
-

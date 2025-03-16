f139996d10d1a772c5c1d0bf95cfc7f2da22e6e1
nthabiseng mahlangu <nthabisengmahlangu139@gmail.com>
2025-03-16 22:24:26 +0200

    Update App.jsx

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,6 +1,6 @@
 // App.jsx
 import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Profile from './Profile';
 import BlogPost from './BlogPost';
 

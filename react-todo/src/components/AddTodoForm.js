759725c38dd0a2799f5b6185f947ea27fec43ae2
nthabiseng mahlangu <nthabisengmahlangu139@gmail.com>
2025-03-17 00:22:09 +0200

    Create AddTodoForm.js

---
+++ react-todo/src/components/AddTodoForm.js
@@ -0,0 +1,26 @@
+// src/components/AddTodoForm.js
+import React, { useState } from 'react';
+
+const AddTodoForm = ({ addTodo }) => {
+  const [text, setText] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    addTodo({ id: Date.now(), text, completed: false });
+    setText('');
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+        placeholder="Add new todo"
+      />
+      <button type="submit">Add Todo</button>
+    </form>
+  );
+};
+
+export default AddTodoForm;

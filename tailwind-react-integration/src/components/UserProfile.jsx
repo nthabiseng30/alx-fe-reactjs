15313b1c526be873c0dd5bcc664eb10721d5b78b
nthabiseng mahlangu <nthabisengmahlangu139@gmail.com>
2025-03-20 17:01:12 +0200

    Update UserProfile.jsx

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function UserProfile() {
   return (
     <div className="user-profile bg-gray-100 sm:p-4 md:p-8 max-w-xs sm:max-w-sm md:max-w-sm mx-auto my-20 rounded-lg shadow-lg">
-      <img src="https://via.placeholder.com/150" alt="User" className="rounded-full w-24 sm:w-36 h-24 sm:h-36" />
+      <img src="https://via.placeholder.com/150" alt="User" className="rounded-full w-20 sm:w-24 sm:h-24 md:w-36 md:h-36" />
       <h1 className="text-lg md:text-xl text-blue-800 my-4">John Doe</h1>
       <p className="text-sm sm:text-base md:text-lg text-gray-600">Developer at Example Co. Loves to write code and explore new technologies.</p>
     </div>

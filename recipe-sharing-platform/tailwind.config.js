f46be9bc9217096fe70598f8604d060c3f2a0f69
nthabiseng mahlangu <nthabisengmahlangu139@gmail.com>
2025-03-21 15:40:32 +0200

    Create tailwind.config.js

---
+++ recipe-sharing-platform/tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  purge: {
+    enabled: process.env.NODE_ENV === 'production',
+    content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  },
+};
+

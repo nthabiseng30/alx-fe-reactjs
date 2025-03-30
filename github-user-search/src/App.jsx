8302e95173c157632aac6989e7f8ec5dafb4e44a
nthabiseng mahlangu <nthabisengmahlangu139@gmail.com>
2025-03-30 02:45:40 +0200

    Update App.jsx

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,23 +1,10 @@
-import { useState } from 'react';
-import './App.css';
+import React from "react";
+import UserProfile from "./components/UserProfile";
 
 function App() {
-  const [searchQuery, setSearchQuery] = useState('');
-
-  const handleSearch = (event) => {
-    setSearchQuery(event.target.value);
-  };
-
   return (
     <div>
-      <h1>GitHub User Search</h1>
-      <input
-        type="search"
-        value={searchQuery}
-        onChange={handleSearch}
-        placeholder="Search for a GitHub user"
-      />
-      {/* Add search results component here */}
+      <UserProfile username="octocat" />
     </div>
   );
 }

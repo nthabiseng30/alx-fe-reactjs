0819cb4bd3ec71148666f4437928c67d5bdc0602
nthabiseng mahlangu <nthabisengmahlangu139@gmail.com>
2025-03-30 02:42:55 +0200

    Update github.jsx

--- github-user-search/src/services/github.jsx
+++ github-user-search/src/services/github.jsx
@@ -1,1 +1,19 @@
+import { Octokit } from "@octokit/octokit";
+
+const octokit = new Octokit({
+  auth: import.meta.env.VITE_APP_GITHUB_API_KEY,
+});
+
+const getUserData = async (username) => {
+  try {
+    const response = await octokit.rest.users.getByUsername({
+      username,
+    });
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export { getUserData };
 
